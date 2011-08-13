5511f2161d71ac67a9bc140cbbca5d9637ab7499
Elliott Hughes <enh@google.com>
2011-08-12 18:07:45 -0700

    Add a valgrind-art-host target to run the tests under valgrind.
    Change-Id: I15b909891492776ff24e62423deadeaa72c38b18

--- Android.mk
+++ Android.mk
@@ -44,6 +44,11 @@ test-art: test-art-host test-art-target
 test-art-host: $(ART_HOST_TEST_EXECUTABLES) $(ANDROID_HOST_OUT)/framework/core-hostdex.jar
 	$(foreach file,$(ART_HOST_TEST_EXECUTABLES),$(file) &&) true
 
+# "mm valgrind-art-host" to build and run all host tests under valgrind.
+.PHONY: valgrind-art-host
+valgrind-art-host: $(ART_HOST_TEST_EXECUTABLES) $(ANDROID_HOST_OUT)/framework/core-hostdex.jar
+	$(foreach file,$(ART_HOST_TEST_EXECUTABLES),valgrind $(file) &&) true
+
 # "mm test-art-device" to build and run all target tests
 .PHONY: test-art-target
 test-art-target: $(ART_TARGET_TEST_EXECUTABLES) $(ANDROID_PRODUCT_OUT)/system/framework/core.jar
