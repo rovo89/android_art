881dd56c9b6ea2c922956650b763d36d2eeb799b
Brian Carlstrom <bdc@google.com>
2011-08-30 09:09:07 -0700

    Reduce test dependencies
    Change-Id: I4acdadb38c91ba76c88d90da52a883c3d20cbe88

--- build/Android.test.mk
+++ build/Android.test.mk
@@ -63,6 +63,8 @@ define build-art-test-dex
   LOCAL_MODULE := art-test-dex-$(1)
   LOCAL_MODULE_TAGS := optional
   LOCAL_SRC_FILES := $(call all-java-files-under, test/$(1))
+  LOCAL_JAVA_LIBRARIES := core
+  LOCAL_NO_STANDARD_LIBRARIES := true
   include $(BUILD_JAVA_LIBRARY)
   ART_TEST_DEX_FILES += $(TARGET_OUT_JAVA_LIBRARIES)/$$(LOCAL_MODULE).jar
 endef
