2faa5f1271587cda765f26bcf2951065300a01ff
Elliott Hughes <enh@google.com>
2012-01-30 14:42:07 -0800

    Fix copyright headers.
    Change-Id: I5b7bc76a370238d810d78522bd5531600746700f

--- src/card_table.h
+++ src/card_table.h
@@ -1,9 +1,17 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
-
 /*
- * Maintain a card table from the the write barrier. All writes of
- * non-NULL values to heap addresses should go through an entry in
- * WriteBarrier, and from there to here.
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef DALVIK_ALLOC_CARDTABLE_H_
@@ -23,6 +31,9 @@ class Object;
 #define GC_CARD_CLEAN 0
 #define GC_CARD_DIRTY 0x70
 
+// Maintain a card table from the the write barrier. All writes of
+// non-NULL values to heap addresses should go through an entry in
+// WriteBarrier, and from there to here.
 class CardTable {
  public:
 
