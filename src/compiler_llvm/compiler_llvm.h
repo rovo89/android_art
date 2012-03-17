0c215dbd2779afc8ee1ee7149ffe6acfffe46fc6
Logan Chien <loganchien@google.com>
2012-03-17 21:46:49 +0800

    Code cleanup: Remove LLVM initialization dead code.
    (cherry picked from commit 446034b2c095c19272388ce4847b453436487dce)
    
    Change-Id: I0d6fb282e5aee56f229d3445c91b270ebbf6ffdc

--- src/compiler_llvm/compiler_llvm.h
+++ src/compiler_llvm/compiler_llvm.h
@@ -107,13 +107,6 @@ class CompilerLLVM {
 
   std::string bitcode_filename_;
 
-  class LLVMInitializer {
-   public:
-    LLVMInitializer();
-    ~LLVMInitializer();
-  };
-  static LLVMInitializer llvm_initialize_guard;
-
   DISALLOW_COPY_AND_ASSIGN(CompilerLLVM);
 };
 
