4b07f172d98846ca6b04d592ea8fd1a1659ada96
Ian Rogers <irogers@google.com>
2013-02-07 08:29:42 -0800

    Add some comments to describe assembly #defines.
    Change-Id: I2e9250a5ae522368ff5abe18e5fd13c8abe94f32

--- src/asm_support.h
+++ src/asm_support.h
@@ -17,22 +17,29 @@
 #ifndef ART_SRC_ASM_SUPPORT_H_
 #define ART_SRC_ASM_SUPPORT_H_
 
+// Value loaded into rSUSPEND for quick. When this value is counted down to zero we do a suspend
+// check.
 #define SUSPEND_CHECK_INTERVAL (1000)
 
+// Offsets within java.lang.String.
 #define STRING_VALUE_OFFSET 8
 #define STRING_COUNT_OFFSET 12
 #define STRING_OFFSET_OFFSET 20
 #define STRING_DATA_OFFSET 12
 
 #if defined(__arm__)
+// Register holding suspend check count down.
 #define rSUSPEND r4
+// Register holding Thread::Current().
 #define rSELF r9
 // Offset of field Thread::suspend_count_ verified in InitCpu
 #define THREAD_FLAGS_OFFSET 0
 // Offset of field Thread::exception_ verified in InitCpu
 #define THREAD_EXCEPTION_OFFSET 12
 #elif defined(__mips__)
+// Register holding suspend check count down.
 #define rSUSPEND $s0
+// Register holding Thread::Current().
 #define rSELF $s1
 // Offset of field Thread::suspend_count_ verified in InitCpu
 #define THREAD_FLAGS_OFFSET 0
