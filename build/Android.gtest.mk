818f2107e6d2d9e80faac8ae8c92faffa83cbd11
Nicolas Geoffray <ngeoffray@google.com>
2014-02-18 16:43:35 +0000

    Re-apply: Initial check-in of an optimizing compiler.
    The classes and the names are very much inspired by V8/Dart.
    It currently only supports the RETURN_VOID dex instruction,
    and there is a pretty printer to check if the building of the
    graph is correct.
    
    Change-Id: I28e125dfee86ae6ec9b3fec6aa1859523b92a893

--- build/Android.gtest.mk
+++ build/Android.gtest.mk
@@ -17,14 +17,15 @@
 LOCAL_PATH := art
 
 TEST_COMMON_SRC_FILES := \
-	compiler/dex/arena_allocator_test.cc \
 	compiler/driver/compiler_driver_test.cc \
 	compiler/elf_writer_test.cc \
 	compiler/image_test.cc \
 	compiler/jni/jni_compiler_test.cc \
 	compiler/leb128_encoder_test.cc \
 	compiler/oat_test.cc \
+	compiler/optimizing/pretty_printer_test.cc \
 	compiler/output_stream_test.cc \
+	compiler/utils/arena_allocator_test.cc \
 	compiler/utils/dedupe_set_test.cc \
 	compiler/utils/arm/managed_register_arm_test.cc \
 	compiler/utils/x86/managed_register_x86_test.cc \
