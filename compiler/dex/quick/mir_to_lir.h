multi-os-engine/art

Allow patching between dex files in the boot classpath.
Change-Id: I53f219a5382d0fcd580e96e50025fdad4fc399df

compiler/dex/quick/mir_to_lir.h
@@ -751,22 +751,22 @@ class Mir2Lir : public Backend {
 
     /*
      * @brief Load the address of the dex method into the register.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @param register that will contain the code address.
      * @note register will be passed to TargetReg to get physical register.
      */
-    void LoadCodeAddress(int dex_method_index, InvokeType type,
+    void LoadCodeAddress(const MethodReference& method, InvokeType type,
                          SpecialTargetRegister symbolic_reg);
 
     /*
      * @brief Load the Method* of a dex method into the register.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @param register that will contain the code address.
      * @note register will be passed to TargetReg to get physical register.
      */
-    virtual void LoadMethodAddress(int dex_method_index, InvokeType type,
+    virtual void LoadMethodAddress(const MethodReference& dex_method_index, InvokeType type,
                                    SpecialTargetRegister symbolic_reg);
 
     /*

compiler/dex/quick/x86/codegen_x86.h
@@ -274,12 +274,12 @@ class X86Mir2Lir : public Mir2Lir {
 
     /*
      * @brief Load the Method* of a dex method into the register.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @param register that will contain the code address.
      * @note register will be passed to TargetReg to get physical register.
      */
-    void LoadMethodAddress(int dex_method_index, InvokeType type,
+    void LoadMethodAddress(const MethodReference& method, InvokeType type,
                            SpecialTargetRegister symbolic_reg);
 
     /*
@@ -292,11 +292,11 @@ class X86Mir2Lir : public Mir2Lir {
 
     /*
      * @brief Generate a relative call to the method that will be patched at link time.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @returns Call instruction
      */
-    LIR * CallWithLinkerFixup(int dex_method_index, InvokeType type);
+    LIR * CallWithLinkerFixup(const MethodReference& method, InvokeType type);
 
     /*
      * @brief Handle x86 specific literals

rovo89/android_art

compiler/dex/quick/mir_to_lir.h
@@ -751,22 +751,22 @@ class Mir2Lir : public Backend {
 
     /*
      * @brief Load the address of the dex method into the register.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param target_method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @param register that will contain the code address.
      * @note register will be passed to TargetReg to get physical register.
      */
-    void LoadCodeAddress(int dex_method_index, InvokeType type,
+    void LoadCodeAddress(const MethodReference& target_method, InvokeType type,
                          SpecialTargetRegister symbolic_reg);
 
     /*
      * @brief Load the Method* of a dex method into the register.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param target_method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @param register that will contain the code address.
      * @note register will be passed to TargetReg to get physical register.
      */
-    virtual void LoadMethodAddress(int dex_method_index, InvokeType type,
+    virtual void LoadMethodAddress(const MethodReference& target_method, InvokeType type,
                                    SpecialTargetRegister symbolic_reg);
 
     /*

compiler/dex/quick/x86/codegen_x86.h
@@ -274,12 +274,12 @@ class X86Mir2Lir : public Mir2Lir {
 
     /*
      * @brief Load the Method* of a dex method into the register.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param target_method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @param register that will contain the code address.
      * @note register will be passed to TargetReg to get physical register.
      */
-    void LoadMethodAddress(int dex_method_index, InvokeType type,
+    void LoadMethodAddress(const MethodReference& target_method, InvokeType type,
                            SpecialTargetRegister symbolic_reg);
 
     /*
@@ -292,11 +292,11 @@ class X86Mir2Lir : public Mir2Lir {
 
     /*
      * @brief Generate a relative call to the method that will be patched at link time.
-     * @param dex_method_index The index of the method to be invoked.
+     * @param target_method The MethodReference of the method to be invoked.
      * @param type How the method will be invoked.
      * @returns Call instruction
      */
-    LIR * CallWithLinkerFixup(int dex_method_index, InvokeType type);
+    LIR * CallWithLinkerFixup(const MethodReference& target_method, InvokeType type);
 
     /*
      * @brief Handle x86 specific literals