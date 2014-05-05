052664322767de1799f060c306e729b0e73ce893
Dave Allison <dallison@google.com>
2014-05-05 13:17:37 -0700

    Switch on implicit null pointer and stack overflow checks.
    This switches on the use of implicit checks for null pointers
    and stack overflows.  These use the SIGSEGV handler in the ART
    runtime to detect these conditions and throw the appropriate
    exceptions.
    
    Change-Id: I4d27d4e976a58b18fb16a89572b1fb3798608449

--- runtime/parsed_options.cc
+++ runtime/parsed_options.cc
@@ -253,7 +253,7 @@ bool ParsedOptions::Parse(const Runtime::Options& options, bool ignore_unrecogni
 #ifdef HAVE_ANDROID_OS
   {
     char buf[PROP_VALUE_MAX];
-    property_get("dalvik.vm.implicit_checks", buf, "none");
+    property_get("dalvik.vm.implicit_checks", buf, "null,stack");
     std::string checks(buf);
     std::vector<std::string> checkvec;
     Split(checks, ',', checkvec);
