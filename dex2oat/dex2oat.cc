136aaee2993a4a5fd93eb9371584161c6d1c7445
Dmitry Petrochenko <dmitry.petrochenko@intel.com>
2014-06-06 15:18:14 +0700

    x86_64: Enable compilation
    This patch enables compilation of all methods for x86_64
    except image.
    
    Change-Id: Ie210809f2595cc25da688a4ad0363c258bcf8233
    Signed-off-by: Dmitry Petrochenko <dmitry.petrochenko@intel.com>

--- dex2oat/dex2oat.cc
+++ dex2oat/dex2oat.cc
@@ -1084,7 +1084,7 @@ static int dex2oat(int argc, char** argv) {
   }
 
   if (compiler_filter_string == nullptr) {
-    if (instruction_set == kX86_64 || instruction_set == kArm64 || instruction_set == kMips) {
+    if ((instruction_set == kX86_64 && image) || instruction_set == kArm64 || instruction_set == kMips) {
       // TODO: implement/fix compilers for these architectures.
       compiler_filter_string = "interpret-only";
     } else if (image) {
