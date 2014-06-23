2073e7553d1ca65f13b6c4e4fe364c2a345a1511
Andreas Gampe <agampe@google.com>
2014-06-23 15:39:00 +0000

    Revert "Revert "ART: Fix StoreValue to use RefDisp when necessary.""
    This reverts commit e98297bafe84e2ac15eb73739f27826fcaae1203.
    
    No matter if holding a reference or not, the store needs to be tagged
    as updating a Dalvik register.
    
    Change-Id: Icb80e1b6f82103cff33ee971faaaa02f9ad4a683

--- compiler/dex/quick/gen_loadstore.cc
+++ compiler/dex/quick/gen_loadstore.cc
@@ -214,7 +214,11 @@ void Mir2Lir::StoreValue(RegLocation rl_dest, RegLocation rl_src) {
   if (IsDirty(rl_dest.reg) && LiveOut(rl_dest.s_reg_low)) {
     def_start = last_lir_insn_;
     ScopedMemRefType mem_ref_type(this, ResourceMask::kDalvikReg);
-    Store32Disp(TargetReg(kSp), SRegOffset(rl_dest.s_reg_low), rl_dest.reg);
+    if (rl_dest.ref) {
+      StoreRefDisp(TargetReg(kSp), SRegOffset(rl_dest.s_reg_low), rl_dest.reg);
+    } else {
+      Store32Disp(TargetReg(kSp), SRegOffset(rl_dest.s_reg_low), rl_dest.reg);
+    }
     MarkClean(rl_dest);
     def_end = last_lir_insn_;
     if (!rl_dest.ref) {
