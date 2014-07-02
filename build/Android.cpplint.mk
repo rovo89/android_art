e130ee6fe902315062ce17f8ce115b1dc506576d
Brian Carlstrom <bdc@google.com>
2014-07-01 23:54:20 -0700

    Add a local elf.h
    Change-Id: Ic2d449bfdd3c95e393c083c9e1022adc4e9c06b8

--- build/Android.cpplint.mk
+++ build/Android.cpplint.mk
@@ -18,7 +18,7 @@ include art/build/Android.common_build.mk
 
 ART_CPPLINT := art/tools/cpplint.py
 ART_CPPLINT_FILTER := --filter=-whitespace/line_length,-build/include,-readability/function,-readability/streams,-readability/todo,-runtime/references,-runtime/sizeof,-runtime/threadsafe_fn,-runtime/printf
-ART_CPPLINT_SRC := $(shell find art -name "*.h" -o -name "*$(ART_CPP_EXTENSION)" | grep -v art/compiler/llvm/generated/)
+ART_CPPLINT_SRC := $(shell find art -name "*.h" -o -name "*$(ART_CPP_EXTENSION)" | grep -v art/compiler/llvm/generated/ | grep -v art/runtime/elf\.h)
 
 # "mm cpplint-art" to verify we aren't regressing
 .PHONY: cpplint-art
