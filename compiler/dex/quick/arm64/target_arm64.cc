multi-os-engine/art

ART: Rework TargetReg(symbolic_reg, wide)
Make the standard implementation in Mir2Lir and the specialized one
in the x86 backend return a pair when wide = "true". Introduce
WideKind enumeration to improve code readability. Simplify generic
code based on this implementation.

Change-Id: I670d45aa2572eedfdc77ac763e6486c83f8e26b4

compiler/dex/quick/arm64/target_arm64.cc
@@ -1108,7 +1108,7 @@ int Arm64Mir2Lir::GenDalvikArgsRange(CallInfo* info, int call_state,
 
       // Instead of allocating a new temp, simply reuse one of the registers being used
       // for argument passing.
-      RegStorage temp = TargetReg(kArg3, false);
+      RegStorage temp = TargetReg(kArg3, kWide32);
 
       // Now load the argument VR and store to the outs.
       Load32Disp(TargetReg(kSp), current_src_offset, temp);

compiler/dex/reg_storage.h
@@ -18,6 +18,7 @@
 #define ART_COMPILER_DEX_REG_STORAGE_H_
 
 #include "base/logging.h"
+#include "compiler_enums.h"  // For WideKind
 
 namespace art {
 
@@ -147,6 +148,10 @@ class RegStorage {
 
   constexpr bool Is64Bit() const {
     return ((reg_ & k64BitMask) == k64Bits);
+  }
+
+  constexpr WideKind GetWideKind() const {
+    return Is64Bit() ? kWide64 : kWide32;
   }
 
   constexpr bool Is64BitSolo() const {

rovo89/android_art

compiler/dex/quick/arm64/target_arm64.cc
@@ -1111,7 +1111,7 @@ int Arm64Mir2Lir::GenDalvikArgsRange(CallInfo* info, int call_state,
 
       // Instead of allocating a new temp, simply reuse one of the registers being used
       // for argument passing.
-      RegStorage temp = TargetReg(kArg3, false);
+      RegStorage temp = TargetReg(kArg3, kNotWide);
 
       // Now load the argument VR and store to the outs.
       Load32Disp(TargetReg(kSp), current_src_offset, temp);

compiler/dex/reg_storage.h
@@ -18,6 +18,7 @@
 #define ART_COMPILER_DEX_REG_STORAGE_H_
 
 #include "base/logging.h"
+#include "compiler_enums.h"  // For WideKind
 
 namespace art {
 
@@ -147,6 +148,10 @@ class RegStorage {
 
   constexpr bool Is64Bit() const {
     return ((reg_ & k64BitMask) == k64Bits);
+  }
+
+  constexpr WideKind GetWideKind() const {
+    return Is64Bit() ? kWide : kNotWide;
   }
 
   constexpr bool Is64BitSolo() const {