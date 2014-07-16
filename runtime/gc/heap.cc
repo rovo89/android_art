b5f5649ba4c8cb51ce143ddbedd5e1549f08f8b3
Stephen Hines <srhines@google.com>
2014-07-15 21:41:06 -0700

    Remove errors/warnings from tautological comparisons.
    Change-Id: I2728b08d55abccd14c111c6c9da93068d4e14275

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -680,15 +680,11 @@ void Heap::RemoveSpace(space::Space* space) {
 }
 
 void Heap::RegisterGCAllocation(size_t bytes) {
-  if (this != nullptr) {
-    gc_memory_overhead_.FetchAndAddSequentiallyConsistent(bytes);
-  }
+  gc_memory_overhead_.FetchAndAddSequentiallyConsistent(bytes);
 }
 
 void Heap::RegisterGCDeAllocation(size_t bytes) {
-  if (this != nullptr) {
-    gc_memory_overhead_.FetchAndSubSequentiallyConsistent(bytes);
-  }
+  gc_memory_overhead_.FetchAndSubSequentiallyConsistent(bytes);
 }
 
 void Heap::DumpGcPerformanceInfo(std::ostream& os) {
@@ -1057,9 +1053,6 @@ void Heap::DumpSpaces(std::ostream& stream) {
 }
 
 void Heap::VerifyObjectBody(mirror::Object* obj) {
-  if (this == nullptr && verify_object_mode_ == kVerifyObjectModeDisabled) {
-    return;
-  }
   // Ignore early dawn of the universe verifications.
   if (UNLIKELY(static_cast<size_t>(num_bytes_allocated_.LoadRelaxed()) < 10 * KB)) {
     return;
