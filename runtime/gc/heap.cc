22c6a817ed3019455d22df13bd91faf012177ef6
Stephen Hines <srhines@google.com>
2014-07-16 11:03:43 -0700

    Add back a deleted check related to verification.
    Change-Id: Ib939d1a7395a4b6cc9a7256cfb0a3498ba296744

--- runtime/gc/heap.cc
+++ runtime/gc/heap.cc
@@ -1053,6 +1053,10 @@ void Heap::DumpSpaces(std::ostream& stream) {
 }
 
 void Heap::VerifyObjectBody(mirror::Object* obj) {
+  if (verify_object_mode_ == kVerifyObjectModeDisabled) {
+    return;
+  }
+
   // Ignore early dawn of the universe verifications.
   if (UNLIKELY(static_cast<size_t>(num_bytes_allocated_.LoadRelaxed()) < 10 * KB)) {
     return;
