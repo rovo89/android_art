f9d6aede77c700118e225f8312cd888262b77862
Vladimir Marko <vmarko@google.com>
2014-07-17 10:43:08 +0100

    Use vabs/fabs on arm/arm64 for intrinsic abs().
    Bug: 11579369
    
    (cherry picked from 5030d3ee8c6fe10394912ede107cbc8df63b7b16)
    
    Change-Id: I7b0596a8e7e3c87a93b225519c5aeedfe4f22e6d

--- compiler/dex/quick/mips/codegen_mips.h
+++ compiler/dex/quick/mips/codegen_mips.h
@@ -110,6 +110,8 @@ class MipsMir2Lir FINAL : public Mir2Lir {
     void GenCmpFP(Instruction::Code opcode, RegLocation rl_dest, RegLocation rl_src1,
                   RegLocation rl_src2);
     void GenConversion(Instruction::Code opcode, RegLocation rl_dest, RegLocation rl_src);
+    bool GenInlinedAbsFloat(CallInfo* info) OVERRIDE;
+    bool GenInlinedAbsDouble(CallInfo* info) OVERRIDE;
     bool GenInlinedCas(CallInfo* info, bool is_long, bool is_object);
     bool GenInlinedMinMax(CallInfo* info, bool is_min, bool is_long);
     bool GenInlinedSqrt(CallInfo* info);
