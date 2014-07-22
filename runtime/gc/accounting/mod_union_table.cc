f85c2fb317399ab540854cd7551ac47690366543
Mathieu Chartier <mathieuc@google.com>
2014-07-22 01:07:36 +0000

    Revert "Disable adding main and non moving spaces to immune region in GSS"
    Bug: 16399257
    
    This reverts commit be0562fb14e6754ee932b8d9c97e2a6df3a91119.
    
    Change-Id: I29e07a8fa1e972990e5bf1ddf8c9a3538ea5f9cf

--- runtime/gc/accounting/mod_union_table.cc
+++ runtime/gc/accounting/mod_union_table.cc
@@ -185,7 +185,7 @@ class CheckReferenceVisitor {
           << from_space->GetGcRetentionPolicy();
       LOG(INFO) << "ToSpace " << to_space->GetName() << " type "
           << to_space->GetGcRetentionPolicy();
-      heap->DumpSpaces(LOG(INFO));
+      heap->DumpSpaces();
       LOG(FATAL) << "FATAL ERROR";
     }
   }
