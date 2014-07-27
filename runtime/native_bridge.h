multi-os-engine/art

ART: Native bridge command-line parameter
Add a command-line parameter for the native bridge library, slight
refactor/cleanup.

Add run-test 115 to test the native bridge interface. Currently the
tests are black-listed for the target, as the setup for the test
is too complicated in the current infrastructure.

Change-Id: I6ccf19485e8c30b96e9f2fd5425278cb1ebd403f

runtime/native_bridge.h
@@ -19,23 +19,38 @@
 
 #include "base/mutex.h"
 
+#include <string>
+
 namespace art {
 
 struct NativeBridgeCallbacks;
 
 class NativeBridge {
  public:
+  // Initialize the native bridge, if any. Should be called by Runtime::Init(). An empty string
+  // signals that we do not want to load a native bridge.
+  static void SetNativeLibraryString(std::string& native_bridge_string);
+
   // Load a shared library that is supported by the native-bridge.
   static void* LoadLibrary(const char* libpath, int flag);
   // Get a native-bridge trampoline for specified native method.
   static void* GetTrampoline(void* handle, const char* name, const char* shorty, uint32_t len);
   // True if native library is valid and is for an ABI that is supported by native-bridge.
-  static bool  IsSupported(const char* libpath);
+  static bool IsSupported(const char* libpath);
 
  private:
-  static bool  Init();
-  static bool  initialized_ GUARDED_BY(lock_);
+  static bool Initialize();
+
+  // The library name we are supposed to load.
+  static std::string native_bridge_string_;
+
+  // Whether we have already initialized (or tried to).
+  static bool initialized_ GUARDED_BY(lock_);
   static Mutex lock_;
+
+  // Whether a native bridge is available (loaded and ready).
+  static bool available_;
+
   static NativeBridgeCallbacks* callbacks_;
 };
 

runtime/parsed_options.cc
@@ -596,6 +596,10 @@ bool ParsedOptions::Parse(const RuntimeOptions& options, bool ignore_unrecognize
         Usage("Unknown -Xverify option %s\n", verify_mode.c_str());
         return false;
       }
+    } else if (StartsWith(option, "-XX:NativeBridge=")) {
+      if (!ParseStringAfterChar(option, '=', &native_bridge_string_)) {
+        return false;
+      }
     } else if (StartsWith(option, "-ea") ||
                StartsWith(option, "-da") ||
                StartsWith(option, "-enableassertions") ||

runtime/parsed_options.h
@@ -45,6 +45,7 @@ class ParsedOptions {
   std::string image_;
   bool check_jni_;
   std::string jni_trace_;
+  std::string native_bridge_string_;
   CompilerCallbacks* compiler_callbacks_;
   bool is_zygote_;
   // TODO Change this to true when we want it on by default.

runtime/runtime.cc
@@ -63,6 +63,7 @@
 #include "mirror/stack_trace_element.h"
 #include "mirror/throwable.h"
 #include "monitor.h"
+#include "native_bridge.h"
 #include "parsed_options.h"
 #include "oat_file.h"
 #include "quick/quick_method_frame_info.h"
@@ -717,6 +718,9 @@ bool Runtime::Init(const RuntimeOptions& raw_options, bool ignore_unrecognized) 
                           "no stack available");
   pre_allocated_OutOfMemoryError_ = self->GetException(NULL);
   self->ClearException();
+
+  // Look for a native bridge.
+  NativeBridge::SetNativeLibraryString(options->native_bridge_string_);
 
   VLOG(startup) << "Runtime::Init exiting";
   return true;

rovo89/android_art

runtime/native_bridge.h
@@ -19,23 +19,38 @@
 
 #include "base/mutex.h"
 
+#include <string>
+
 namespace art {
 
 struct NativeBridgeCallbacks;
 
 class NativeBridge {
  public:
+  // Initialize the native bridge, if any. Should be called by Runtime::Init(). An empty string
+  // signals that we do not want to load a native bridge.
+  static void SetNativeBridgeLibraryString(std::string& native_bridge_library_string);
+
   // Load a shared library that is supported by the native-bridge.
   static void* LoadLibrary(const char* libpath, int flag);
   // Get a native-bridge trampoline for specified native method.
   static void* GetTrampoline(void* handle, const char* name, const char* shorty, uint32_t len);
   // True if native library is valid and is for an ABI that is supported by native-bridge.
-  static bool  IsSupported(const char* libpath);
+  static bool IsSupported(const char* libpath);
 
  private:
-  static bool  Init();
-  static bool  initialized_ GUARDED_BY(lock_);
+  static bool Initialize();
+
+  // The library name we are supposed to load.
+  static std::string native_bridge_library_string_;
+
+  // Whether we have already initialized (or tried to).
+  static bool initialized_ GUARDED_BY(lock_);
   static Mutex lock_;
+
+  // Whether a native bridge is available (loaded and ready).
+  static bool available_;
+
   static NativeBridgeCallbacks* callbacks_;
 };
 

runtime/parsed_options.cc
@@ -596,6 +596,10 @@ bool ParsedOptions::Parse(const RuntimeOptions& options, bool ignore_unrecognize
         Usage("Unknown -Xverify option %s\n", verify_mode.c_str());
         return false;
       }
+    } else if (StartsWith(option, "-XX:NativeBridge=")) {
+      if (!ParseStringAfterChar(option, '=', &native_bridge_library_string_)) {
+        return false;
+      }
     } else if (StartsWith(option, "-ea") ||
                StartsWith(option, "-da") ||
                StartsWith(option, "-enableassertions") ||

runtime/parsed_options.h
@@ -45,6 +45,7 @@ class ParsedOptions {
   std::string image_;
   bool check_jni_;
   std::string jni_trace_;
+  std::string native_bridge_library_string_;
   CompilerCallbacks* compiler_callbacks_;
   bool is_zygote_;
   // TODO Change this to true when we want it on by default.

runtime/runtime.cc
@@ -63,6 +63,7 @@
 #include "mirror/stack_trace_element.h"
 #include "mirror/throwable.h"
 #include "monitor.h"
+#include "native_bridge.h"
 #include "parsed_options.h"
 #include "oat_file.h"
 #include "quick/quick_method_frame_info.h"
@@ -717,6 +718,9 @@ bool Runtime::Init(const RuntimeOptions& raw_options, bool ignore_unrecognized) 
                           "no stack available");
   pre_allocated_OutOfMemoryError_ = self->GetException(NULL);
   self->ClearException();
+
+  // Look for a native bridge.
+  NativeBridge::SetNativeBridgeLibraryString(options->native_bridge_library_string_);
 
   VLOG(startup) << "Runtime::Init exiting";
   return true;