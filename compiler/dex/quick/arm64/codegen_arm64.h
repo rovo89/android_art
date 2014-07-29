f29ecd69af2743a425314baa4abd6c44d8d88649
Andreas Gampe <agampe@google.com>
2014-07-29 00:35:00 -0700

    ART: Rework ARM64 entry sequence
    Try to fold one sub of SP in the ARM64 entry sequence. When the
    framesize is small, generate a sub over the full frame-size, and
    adjust the spill offsets accordingly. If the framesize is too
    large, use a pre-indexed store and fill upwards from there.
    
    Change-Id: I1c15ac6276fb62b8164372de02fd92437f605938

--- compiler/dex/quick/arm64/codegen_arm64.h
+++ compiler/dex/quick/arm64/codegen_arm64.h
@@ -219,11 +219,12 @@ class Arm64Mir2Lir FINAL : public Mir2Lir {
     void GenPackedSwitch(MIR* mir, DexOffset table_offset, RegLocation rl_src);
     void GenSparseSwitch(MIR* mir, DexOffset table_offset, RegLocation rl_src);
 
-    uint32_t GenPairWise(uint32_t reg_mask, int* reg1, int* reg2);
-    void UnSpillCoreRegs(RegStorage base, int offset, uint32_t reg_mask);
-    void SpillCoreRegs(RegStorage base, int offset, uint32_t reg_mask);
-    void UnSpillFPRegs(RegStorage base, int offset, uint32_t reg_mask);
-    void SpillFPRegs(RegStorage base, int offset, uint32_t reg_mask);
+    // Spill core and FP registers. Returns the SP difference: either spill size, or whole
+    // frame size.
+    int SpillRegs(RegStorage base, uint32_t core_reg_mask, uint32_t fp_reg_mask, int frame_size);
+
+    // Unspill core and FP registers.
+    void UnspillRegs(RegStorage base, uint32_t core_reg_mask, uint32_t fp_reg_mask, int frame_size);
 
     // Required for target - single operation generators.
     LIR* OpUnconditionalBranch(LIR* target);
