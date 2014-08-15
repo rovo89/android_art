43c69cc4cea794cd4d89d9d0680b1e25c6d02acc
Ian Rogers <irogers@google.com>
2014-08-15 11:09:28 -0700

    Make Monitor::Wait more robust to spurious Inflate failures.
    Bug: 17062710
    Change-Id: Ife5f6b335caacc70cab543cd568676d277d3beb6
    (cherry picked from commit 6f22fc166ed6c11cad229bff442c064e704de101)

--- runtime/mirror/object.cc
+++ runtime/mirror/object.cc
@@ -162,7 +162,8 @@ int32_t Object::IdentityHashCode() const {
         break;
       }
       case LockWord::kThinLocked: {
-        // Inflate the thin lock to a monitor and stick the hash code inside of the monitor.
+        // Inflate the thin lock to a monitor and stick the hash code inside of the monitor. May
+        // fail spuriously.
         Thread* self = Thread::Current();
         StackHandleScope<1> hs(self);
         Handle<mirror::Object> h_this(hs.NewHandle(current_this));
