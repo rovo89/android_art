6c82d40eb142771086f5531998de2273ba5cc08c
Roland Levillain <rpl@google.com>
2014-10-13 16:10:27 +0100

    Have HInstruction::StrictlyDominates compute strict dominance.
    Change-Id: I3a4fa133268615fb4ce54a0bcb43e0c2458cc865

--- compiler/optimizing/nodes.cc
+++ compiler/optimizing/nodes.cc
@@ -472,7 +472,11 @@ bool HInstructionList::FoundBefore(const HInstruction* instruction1,
   return true;
 }
 
-bool HInstruction::Dominates(HInstruction* other_instruction) const {
+bool HInstruction::StrictlyDominates(HInstruction* other_instruction) const {
+  if (other_instruction == this) {
+    // An instruction does not strictly dominate itself.
+    return false;
+  }
   HBasicBlock* block = GetBlock();
   HBasicBlock* other_block = other_instruction->GetBlock();
   if (block != other_block) {
