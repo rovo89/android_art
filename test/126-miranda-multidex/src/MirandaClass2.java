2b0fa5ba4a8f07ee243452003bf93418d30e9448
Andreas Gampe <agampe@google.com>
2014-10-31 18:12:30 -0700

    ART: Add miranda checking
    Add a test for resolution of miranda methods across dex-files. See
    CL 112688 and b/18193682 for the code change.
    
    Fix the test script to support no-verify again.
    
    Weaken the dex cache check to a check whether the found method is a
    miranda method. This will penalize miranda methods, as they will
    always have to be resolved by name, but does not require the multi-step
    load of the method's dex cache.
    
    Bug: 18193682
    Change-Id: I1dae2a9ec0985dc8625acd7dd5686a5d3cdc8888

--- test/126-miranda-multidex/src/MirandaClass2.java
+++ test/126-miranda-multidex/src/MirandaClass2.java
@@ -22,4 +22,21 @@ class MirandaClass2 extends MirandaAbstract {
     public int inInterface2() {
         return 28;
     }
+
+    // Better not hit any of these...
+    public void inInterfaceDummy1() {
+        System.out.println("inInterfaceDummy1");
+    }
+    public void inInterfaceDummy2() {
+        System.out.println("inInterfaceDummy2");
+    }
+    public void inInterfaceDummy3() {
+        System.out.println("inInterfaceDummy3");
+    }
+    public void inInterfaceDummy4() {
+        System.out.println("inInterfaceDummy4");
+    }
+    public void inInterfaceDummy5() {
+        System.out.println("inInterfaceDummy5");
+    }
 }
