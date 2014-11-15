98b8bcf433e7bd8f1a447f7bf8d6cf80ec505c5e
Dan Albert <danalbert@google.com>
2014-11-14 19:56:21 -0800

    Up the stack size limit for SANITIZE_HOST.
    Change-Id: I7c507a6a2e8de534f0e09e961d13db0223eee891

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -243,7 +243,9 @@ art_target_non_debug_cflags := $(art_non_debug_cflags)
 
 ifeq ($(HOST_OS),linux)
   # Larger frame-size for host clang builds today
-  art_host_non_debug_cflags += -Wframe-larger-than=2600
+  ifndef SANITIZE_HOST
+    art_host_non_debug_cflags += -Wframe-larger-than=2600
+  endif
   art_target_non_debug_cflags += -Wframe-larger-than=1728
 endif
 
