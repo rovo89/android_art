multi-os-engine/art

Enable core callee-save on x64.
Will work on other architectures and FP support in other CLs.

Change-Id: I8cef0343eedc7202d206f5217fdf0349035f0e4d

compiler/optimizing/code_generator_arm.h
@@ -192,7 +192,7 @@ class CodeGeneratorARM : public CodeGenerator {
     return GetLabelOf(block)->Position();
   }
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool baseline) const OVERRIDE;
 
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;
 

compiler/optimizing/code_generator_arm64.cc
@@ -728,7 +728,7 @@ void CodeGeneratorARM64::MarkGCCard(Register object, Register value) {
   __ Bind(&done);
 }
 
-void CodeGeneratorARM64::SetupBlockedRegisters() const {
+void CodeGeneratorARM64::SetupBlockedRegisters(bool baseline ATTRIBUTE_UNUSED) const {
   // Block reserved registers:
   //   ip0 (VIXL temporary)
   //   ip1 (VIXL temporary)

compiler/optimizing/code_generator_arm64.h
@@ -214,7 +214,7 @@ class CodeGeneratorARM64 : public CodeGenerator {
 
   // Register allocation.
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool baseline) const OVERRIDE;
   // AllocateFreeRegister() is only used when allocating registers locally
   // during CompileBaseline().
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;

compiler/optimizing/code_generator_x86.h
@@ -189,7 +189,7 @@ class CodeGeneratorX86 : public CodeGenerator {
     return GetLabelOf(block)->Position();
   }
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool baseline) const OVERRIDE;
 
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;
 

compiler/optimizing/code_generator_x86_64.h
@@ -212,7 +212,7 @@ class CodeGeneratorX86_64 : public CodeGenerator {
 
   Location GetStackLocation(HLoadLocal* load) const OVERRIDE;
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool baseline) const OVERRIDE;
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;
   void DumpCoreRegister(std::ostream& stream, int reg) const OVERRIDE;
   void DumpFloatingPointRegister(std::ostream& stream, int reg) const OVERRIDE;

rovo89/android_art

compiler/optimizing/code_generator_arm.h
@@ -197,7 +197,7 @@ class CodeGeneratorARM : public CodeGenerator {
     return GetLabelOf(block)->Position();
   }
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool is_baseline) const OVERRIDE;
 
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;
 

compiler/optimizing/code_generator_arm64.cc
@@ -567,6 +567,8 @@ CodeGeneratorARM64::CodeGeneratorARM64(HGraph* graph, const CompilerOptions& com
                     kNumberOfAllocatableRegisters,
                     kNumberOfAllocatableFPRegisters,
                     kNumberOfAllocatableRegisterPairs,
+                    0,
+                    0,
                     compiler_options),
       block_labels_(nullptr),
       location_builder_(graph, this),
@@ -729,7 +731,7 @@ void CodeGeneratorARM64::MarkGCCard(Register object, Register value) {
   __ Bind(&done);
 }
 
-void CodeGeneratorARM64::SetupBlockedRegisters() const {
+void CodeGeneratorARM64::SetupBlockedRegisters(bool is_baseline ATTRIBUTE_UNUSED) const {
   // Block reserved registers:
   //   ip0 (VIXL temporary)
   //   ip1 (VIXL temporary)

compiler/optimizing/code_generator_arm64.h
@@ -217,7 +217,7 @@ class CodeGeneratorARM64 : public CodeGenerator {
 
   // Register allocation.
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool is_baseline) const OVERRIDE;
   // AllocateFreeRegister() is only used when allocating registers locally
   // during CompileBaseline().
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;

compiler/optimizing/code_generator_x86.h
@@ -196,7 +196,7 @@ class CodeGeneratorX86 : public CodeGenerator {
     return GetLabelOf(block)->Position();
   }
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool is_baseline) const OVERRIDE;
 
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;
 

compiler/optimizing/code_generator_x86_64.h
@@ -218,7 +218,7 @@ class CodeGeneratorX86_64 : public CodeGenerator {
 
   Location GetStackLocation(HLoadLocal* load) const OVERRIDE;
 
-  void SetupBlockedRegisters() const OVERRIDE;
+  void SetupBlockedRegisters(bool is_baseline) const OVERRIDE;
   Location AllocateFreeRegister(Primitive::Type type) const OVERRIDE;
   void DumpCoreRegister(std::ostream& stream, int reg) const OVERRIDE;
   void DumpFloatingPointRegister(std::ostream& stream, int reg) const OVERRIDE;