72d72ef51aef106d6f0300a72b583bcc5cd90a13
Mathieu Chartier <mathieuc@google.com>
2015-01-28 13:11:44 -0800

    Force set resolved method for static invokes
    For static invokes, we may dispatch to the static method in the
    superclass but resolve using the subclass. To prevent getting slow
    paths on each invoke, we force set the resolved method for the
    super class dex method index if we are in the same dex file.
    
    Added test.
    
    Bug: 19175856
    (cherry picked from commit e4a91bbeaa118bc01a5c338c6a6d9ee094a6a2c6)
    Change-Id: I26f8644a7f725f5c2dc2a94a8e9578f573792507

--- test/Android.run-test.mk
+++ test/Android.run-test.mk
@@ -53,7 +53,8 @@ endef  # all-run-test-target-names
 # Tests that are timing sensitive and flaky on heavily loaded systems.
 TEST_ART_TIMING_SENSITIVE_RUN_TESTS := \
   053-wait-some \
-  055-enum-performance
+  055-enum-performance \
+  133-static-invoke-super
 
  # disable timing sensitive tests on "dist" builds.
 ifdef dist_goal
