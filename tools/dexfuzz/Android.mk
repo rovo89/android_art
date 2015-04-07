733840f23a6983436437d4334f33d5032f494696
Calin Juravle <calin@google.com>
2015-04-07 12:31:39 +0100

    Remove fuzzer build-time dependency on core image.
    Test if the image exists at runtime.
    
    Forcing this dependency slows down development. For example, if after a
    change dex2oat does not compile anymore you need to first revert to a
    stable state and rebuild the core image to be able to move foreward.
    Since this is a test tool, it is ok to assume that the core image has
    already been built if host execution was selected.
    
    Change-Id: Id61a960ba62bb8d1d21acb07114f72208fbc9720

--- tools/dexfuzz/Android.mk
+++ tools/dexfuzz/Android.mk
@@ -31,7 +31,10 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE := dexfuzz
 include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/dexfuzz $(ACP) $(HOST_CORE_IMG_OUTS)
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/dexfuzz $(ACP)
 	@echo "Copy: $(PRIVATE_MODULE) ($@)"
 	$(copy-file-to-new-target)
 	$(hide) chmod 755 $@
+
+# --- dexfuzz script with core image dependencies ----------------
+fuzzer: $(LOCAL_BUILT_MODULE) $(HOST_CORE_IMG_OUTS)
