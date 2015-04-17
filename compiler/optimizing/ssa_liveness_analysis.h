8826f67ad53099021f6442364348fa66729288d7
Nicolas Geoffray <ngeoffray@google.com>
2015-04-17 09:15:11 +0100

    Callee/caller save logic in register allocator.
    Prevent intervals that do not span a 'will-call' safepoint
    to allocate a callee-save register when caller-saves
    are available.
    
    Change-Id: I6e613ab54b087f433bbc433aa62847fbca423377

--- compiler/optimizing/ssa_liveness_analysis.h
+++ compiler/optimizing/ssa_liveness_analysis.h
@@ -542,6 +542,15 @@ class LiveInterval : public ArenaObject<kArenaAllocMisc> {
     return defined_by_;
   }
 
+  bool HasWillCallSafepoint() const {
+    for (SafepointPosition* safepoint = first_safepoint_;
+         safepoint != nullptr;
+         safepoint = safepoint->GetNext()) {
+      if (safepoint->GetLocations()->WillCall()) return true;
+    }
+    return false;
+  }
+
   SafepointPosition* FindSafepointJustBefore(size_t position) const {
     for (SafepointPosition* safepoint = first_safepoint_, *previous = nullptr;
          safepoint != nullptr;
