8d9b6d6a9acd5401d945062072ef2dc4b7cf2d69
Piotr Jastrzebski <haaawk@google.com>
2015-05-05 16:19:00 +0100

    Update the size of Class.class.
    Change-Id: I8e1f224d6d25cdffe561865e107549b7f7cd2e5a

--- runtime/mirror/class.h
+++ runtime/mirror/class.h
@@ -503,7 +503,7 @@ class MANAGED Class FINAL : public Object {
   // The size of java.lang.Class.class.
   static uint32_t ClassClassSize() {
     // The number of vtable entries in java.lang.Class.
-    uint32_t vtable_entries = Object::kVTableLength + 66;
+    uint32_t vtable_entries = Object::kVTableLength + 69;
     return ComputeClassSize(true, vtable_entries, 0, 1, 0);
   }
 
