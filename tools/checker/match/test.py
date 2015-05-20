6423cf5ef0ddf82722a6ed77ad3908fc8802d8cf
David Brazdil <dbrazdil@google.com>
2015-05-20 14:57:54 +0100

    ART: Refactor and simplify matching in Checker
    Change-Id: Ib8a2b51488f66a7239e799f5fa5910b4ac2dfe08

--- tools/checker/match/test.py
+++ tools/checker/match/test.py
@@ -18,7 +18,7 @@ from file_format.c1visualizer.parser import ParseC1visualizerStream
 from file_format.c1visualizer.struct import C1visualizerFile, C1visualizerPass
 from file_format.checker.parser      import ParseCheckerStream, ParseCheckerAssertion
 from file_format.checker.struct      import CheckerFile, TestCase, TestAssertion, RegexExpression
-from match.file                      import MatchFiles
+from match.file                      import MatchTestCase, MatchFailedException
 from match.line                      import MatchLines
 
 import io
@@ -38,68 +38,71 @@ class MatchLines_Test(unittest.TestCase):
                       ToUnicode(c1String),
                       ImmutableDict(varState))
 
-  def matches(self, checkerString, c1String, varState={}):
-    return self.tryMatch(checkerString, c1String, varState) is not None
+  def assertMatches(self, checkerString, c1String, varState={}):
+    self.assertIsNotNone(self.tryMatch(checkerString, c1String, varState))
+
+  def assertDoesNotMatch(self, checkerString, c1String, varState={}):
+    self.assertIsNone(self.tryMatch(checkerString, c1String, varState))
 
   def test_TextAndWhitespace(self):
-    self.assertTrue(self.matches("foo", "foo"))
-    self.assertTrue(self.matches("foo", "  foo  "))
-    self.assertTrue(self.matches("foo", "foo bar"))
-    self.assertFalse(self.matches("foo", "XfooX"))
-    self.assertFalse(self.matches("foo", "zoo"))
+    self.assertMatches("foo", "foo")
+    self.assertMatches("foo", "  foo  ")
+    self.assertMatches("foo", "foo bar")
+    self.assertDoesNotMatch("foo", "XfooX")
+    self.assertDoesNotMatch("foo", "zoo")
 
-    self.assertTrue(self.matches("foo bar", "foo   bar"))
-    self.assertTrue(self.matches("foo bar", "abc foo bar def"))
-    self.assertTrue(self.matches("foo bar", "foo foo bar bar"))
+    self.assertMatches("foo bar", "foo   bar")
+    self.assertMatches("foo bar", "abc foo bar def")
+    self.assertMatches("foo bar", "foo foo bar bar")
 
-    self.assertTrue(self.matches("foo bar", "foo X bar"))
-    self.assertFalse(self.matches("foo bar", "foo Xbar"))
+    self.assertMatches("foo bar", "foo X bar")
+    self.assertDoesNotMatch("foo bar", "foo Xbar")
 
   def test_Pattern(self):
-    self.assertTrue(self.matches("foo{{A|B}}bar", "fooAbar"))
-    self.assertTrue(self.matches("foo{{A|B}}bar", "fooBbar"))
-    self.assertFalse(self.matches("foo{{A|B}}bar", "fooCbar"))
+    self.assertMatches("foo{{A|B}}bar", "fooAbar")
+    self.assertMatches("foo{{A|B}}bar", "fooBbar")
+    self.assertDoesNotMatch("foo{{A|B}}bar", "fooCbar")
 
   def test_VariableReference(self):
-    self.assertTrue(self.matches("foo<<X>>bar", "foobar", {"X": ""}))
-    self.assertTrue(self.matches("foo<<X>>bar", "fooAbar", {"X": "A"}))
-    self.assertTrue(self.matches("foo<<X>>bar", "fooBbar", {"X": "B"}))
-    self.assertFalse(self.matches("foo<<X>>bar", "foobar", {"X": "A"}))
-    self.assertFalse(self.matches("foo<<X>>bar", "foo bar", {"X": "A"}))
+    self.assertMatches("foo<<X>>bar", "foobar", {"X": ""})
+    self.assertMatches("foo<<X>>bar", "fooAbar", {"X": "A"})
+    self.assertMatches("foo<<X>>bar", "fooBbar", {"X": "B"})
+    self.assertDoesNotMatch("foo<<X>>bar", "foobar", {"X": "A"})
+    self.assertDoesNotMatch("foo<<X>>bar", "foo bar", {"X": "A"})
     with self.assertRaises(CheckerException):
-      self.assertTrue(self.matches("foo<<X>>bar", "foobar", {}))
+      self.tryMatch("foo<<X>>bar", "foobar", {})
 
   def test_VariableDefinition(self):
-    self.assertTrue(self.matches("foo<<X:A|B>>bar", "fooAbar"))
-    self.assertTrue(self.matches("foo<<X:A|B>>bar", "fooBbar"))
-    self.assertFalse(self.matches("foo<<X:A|B>>bar", "fooCbar"))
+    self.assertMatches("foo<<X:A|B>>bar", "fooAbar")
+    self.assertMatches("foo<<X:A|B>>bar", "fooBbar")
+    self.assertDoesNotMatch("foo<<X:A|B>>bar", "fooCbar")
 
     env = self.tryMatch("foo<<X:A.*B>>bar", "fooABbar", {})
     self.assertEqual(env, {"X": "AB"})
     env = self.tryMatch("foo<<X:A.*B>>bar", "fooAxxBbar", {})
     self.assertEqual(env, {"X": "AxxB"})
 
-    self.assertTrue(self.matches("foo<<X:A|B>>bar<<X>>baz", "fooAbarAbaz"))
-    self.assertTrue(self.matches("foo<<X:A|B>>bar<<X>>baz", "fooBbarBbaz"))
-    self.assertFalse(self.matches("foo<<X:A|B>>bar<<X>>baz", "fooAbarBbaz"))
+    self.assertMatches("foo<<X:A|B>>bar<<X>>baz", "fooAbarAbaz")
+    self.assertMatches("foo<<X:A|B>>bar<<X>>baz", "fooBbarBbaz")
+    self.assertDoesNotMatch("foo<<X:A|B>>bar<<X>>baz", "fooAbarBbaz")
 
   def test_NoVariableRedefinition(self):
     with self.assertRaises(CheckerException):
-      self.matches("<<X:...>><<X>><<X:...>><<X>>", "foofoobarbar")
+      self.tryMatch("<<X:...>><<X>><<X:...>><<X>>", "foofoobarbar")
 
   def test_EnvNotChangedOnPartialMatch(self):
     env = {"Y": "foo"}
-    self.assertFalse(self.matches("<<X:A>>bar", "Abaz", env))
+    self.assertDoesNotMatch("<<X:A>>bar", "Abaz", env)
     self.assertFalse("X" in env.keys())
 
   def test_VariableContentEscaped(self):
-    self.assertTrue(self.matches("<<X:..>>foo<<X>>", ".*foo.*"))
-    self.assertFalse(self.matches("<<X:..>>foo<<X>>", ".*fooAAAA"))
+    self.assertMatches("<<X:..>>foo<<X>>", ".*foo.*")
+    self.assertDoesNotMatch("<<X:..>>foo<<X>>", ".*fooAAAA")
 
 
 class MatchFiles_Test(unittest.TestCase):
 
-  def matches(self, checkerString, c1String):
+  def assertMatches(self, checkerString, c1String):
     checkerString = \
       """
         // CHECK-START: MyMethod MyPass
@@ -119,22 +122,24 @@ class MatchFiles_Test(unittest.TestCase):
       """
     checkerFile = ParseCheckerStream("<test-file>", "CHECK", io.StringIO(ToUnicode(checkerString)))
     c1File = ParseC1visualizerStream("<c1-file>", io.StringIO(ToUnicode(c1String)))
-    try:
-      MatchFiles(checkerFile, c1File)
-      return True
-    except CheckerException:
-      return False
+    assert len(checkerFile.testCases) == 1
+    assert len(c1File.passes) == 1
+    MatchTestCase(checkerFile.testCases[0], c1File.passes[0])
+
+  def assertDoesNotMatch(self, checkerString, c1String):
+    with self.assertRaises(MatchFailedException):
+      self.assertMatches(checkerString, c1String)
 
   def test_Text(self):
-    self.assertTrue(self.matches( "// CHECK: foo bar", "foo bar"))
-    self.assertFalse(self.matches("// CHECK: foo bar", "abc def"))
+    self.assertMatches("// CHECK: foo bar", "foo bar")
+    self.assertDoesNotMatch("// CHECK: foo bar", "abc def")
 
   def test_Pattern(self):
-    self.assertTrue(self.matches( "// CHECK: abc {{de.}}", "abc de#"))
-    self.assertFalse(self.matches("// CHECK: abc {{de.}}", "abc d#f"))
+    self.assertMatches("// CHECK: abc {{de.}}", "abc de#")
+    self.assertDoesNotMatch("// CHECK: abc {{de.}}", "abc d#f")
 
   def test_Variables(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK: foo<<X:.>>bar
       // CHECK: abc<<X>>def
@@ -142,8 +147,8 @@ class MatchFiles_Test(unittest.TestCase):
     """
       foo0bar
       abc0def
-    """))
-    self.assertTrue(self.matches(
+    """)
+    self.assertMatches(
     """
       // CHECK: foo<<X:([0-9]+)>>bar
       // CHECK: abc<<X>>def
@@ -153,8 +158,8 @@ class MatchFiles_Test(unittest.TestCase):
       foo1234bar
       abc1234def
       ### 1234 ###
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK: foo<<X:([0-9]+)>>bar
       // CHECK: abc<<X>>def
@@ -162,16 +167,16 @@ class MatchFiles_Test(unittest.TestCase):
     """
       foo1234bar
       abc1235def
-    """))
+    """)
 
   def test_WholeWordMustMatch(self):
-    self.assertTrue(self.matches( "// CHECK: b{{.}}r", "abc bar def"))
-    self.assertFalse(self.matches( "// CHECK: b{{.}}r", "abc Xbar def"))
-    self.assertFalse(self.matches( "// CHECK: b{{.}}r", "abc barX def"))
-    self.assertFalse(self.matches( "// CHECK: b{{.}}r", "abc b r def"))
+    self.assertMatches("// CHECK: b{{.}}r", "abc bar def")
+    self.assertDoesNotMatch("// CHECK: b{{.}}r", "abc Xbar def")
+    self.assertDoesNotMatch("// CHECK: b{{.}}r", "abc barX def")
+    self.assertDoesNotMatch("// CHECK: b{{.}}r", "abc b r def")
 
   def test_InOrderAssertions(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK: foo
       // CHECK: bar
@@ -179,8 +184,8 @@ class MatchFiles_Test(unittest.TestCase):
     """
       foo
       bar
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK: foo
       // CHECK: bar
@@ -188,10 +193,10 @@ class MatchFiles_Test(unittest.TestCase):
     """
       bar
       foo
-    """))
+    """)
 
   def test_DagAssertions(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK-DAG: foo
       // CHECK-DAG: bar
@@ -199,8 +204,8 @@ class MatchFiles_Test(unittest.TestCase):
     """
       foo
       bar
-    """))
-    self.assertTrue(self.matches(
+    """)
+    self.assertMatches(
     """
       // CHECK-DAG: foo
       // CHECK-DAG: bar
@@ -208,10 +213,10 @@ class MatchFiles_Test(unittest.TestCase):
     """
       bar
       foo
-    """))
+    """)
 
   def test_DagAssertionsScope(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK:     foo
       // CHECK-DAG: abc
@@ -223,8 +228,8 @@ class MatchFiles_Test(unittest.TestCase):
       def
       abc
       bar
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK:     foo
       // CHECK-DAG: abc
@@ -236,8 +241,8 @@ class MatchFiles_Test(unittest.TestCase):
       abc
       bar
       def
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK:     foo
       // CHECK-DAG: abc
@@ -249,26 +254,26 @@ class MatchFiles_Test(unittest.TestCase):
       def
       bar
       abc
-    """))
+    """)
 
   def test_NotAssertions(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK-NOT: foo
     """,
     """
       abc
       def
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK-NOT: foo
     """,
     """
       abc foo
       def
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK-NOT: foo
       // CHECK-NOT: bar
@@ -276,10 +281,10 @@ class MatchFiles_Test(unittest.TestCase):
     """
       abc
       def bar
-    """))
+    """)
 
   def test_NotAssertionsScope(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK:     abc
       // CHECK-NOT: foo
@@ -288,8 +293,8 @@ class MatchFiles_Test(unittest.TestCase):
     """
       abc
       def
-    """))
-    self.assertTrue(self.matches(
+    """)
+    self.assertMatches(
     """
       // CHECK:     abc
       // CHECK-NOT: foo
@@ -299,8 +304,8 @@ class MatchFiles_Test(unittest.TestCase):
       abc
       def
       foo
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK:     abc
       // CHECK-NOT: foo
@@ -310,10 +315,10 @@ class MatchFiles_Test(unittest.TestCase):
       abc
       foo
       def
-    """))
+    """)
 
   def test_LineOnlyMatchesOnce(self):
-    self.assertTrue(self.matches(
+    self.assertMatches(
     """
       // CHECK-DAG: foo
       // CHECK-DAG: foo
@@ -322,8 +327,8 @@ class MatchFiles_Test(unittest.TestCase):
       foo
       abc
       foo
-    """))
-    self.assertFalse(self.matches(
+    """)
+    self.assertDoesNotMatch(
     """
       // CHECK-DAG: foo
       // CHECK-DAG: foo
@@ -332,4 +337,4 @@ class MatchFiles_Test(unittest.TestCase):
       foo
       abc
       bar
-    """))
+    """)
