aaf143d80bc6f226290eadb10361a752ed5d204e
David Srbecky <dsrbecky@google.com>
2015-05-21 14:03:48 +0100

    Switch to using .debug_frame for CFI.
    This will make it easier to strip or compress CFI using standard tools.
    
    It also saves some space since we do not need .eh_frame_hdr anymore.
    
    Bug:20556771
    Change-Id: I8656b1ff248e4b12cb94924800c3a91fac8bdda4

--- compiler/elf_writer_quick.cc
+++ compiler/elf_writer_quick.cc
@@ -43,7 +43,8 @@ namespace art {
 // it is used by C++ exception handling (which we do not use so we
 // can choose either).  C++ compilers generally tend to use .eh_frame
 // because if they need it sometimes, they might as well always use it.
-constexpr dwarf::CFIFormat kCFIFormat = dwarf::DW_EH_FRAME_FORMAT;
+// Let's use .debug_frame because it is easier to strip or compress.
+constexpr dwarf::CFIFormat kCFIFormat = dwarf::DW_DEBUG_FRAME_FORMAT;
 
 template <typename ElfTypes>
 bool ElfWriterQuick<ElfTypes>::Create(File* elf_file,
