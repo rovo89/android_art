rovo89/android_art

commit 5184a0542b81c297afa97d4540e4751db5fc9876

[Xposed] Don't reuse open oat files from Zygote
Xposed modules are loaded in the Zygote process. ART compiles the .dex file
and opens the resulting .oat file in the Dalvik cache for that. However,
the same happens again when the user installs a new version of the module,
overwriting the file in the Dalvik cache. When opening the new .oat file
though, ART detects that the file is already opened and reuses this instance.
Later, it detects that the checksum doesn't match, which causes a crash.
This will only occur on every second update (without reboot) as Android
uses alternating suffixes for the .apk files (-1.apk / -2.apk).

Let's work around it by ignoring already opened .oat files if they have been
opened in Zygote. The behavior for files in /system (boot image and framework)
is unchanged.

Fixes rovo89/Xposed#22.

runtime/oat_file.cc
@@ -139,7 +139,7 @@ OatFile* OatFile::OpenElfFile(File* file,
 
 OatFile::OatFile(const std::string& location, bool is_executable)
     : location_(location), begin_(NULL), end_(NULL), is_executable_(is_executable),
-      dlopen_handle_(NULL),
+      is_created_in_zygote_(Runtime::Current()->IsZygote()), dlopen_handle_(NULL),
       secondary_lookup_lock_("OatFile secondary lookup lock", kOatFileSecondaryLookupLock) {
   CHECK(!location_.empty());
 }

runtime/class_linker.cc
@@ -1229,6 +1229,10 @@ const OatFile* ClassLinker::FindOpenedOatFileFromOatLocation(const std::string& 
     const OatFile* oat_file = oat_files_[i];
     DCHECK(oat_file != nullptr);
     if (oat_file->GetLocation() == oat_location) {
+      if (oat_file->IsCreatedInZygote() && !Runtime::Current()->IsZygote() && oat_location.find("/system@") == std::string::npos) {
+        LOG(INFO) << "Ignoring oat file opened by Zygote: " << oat_location;
+        continue;
+      }
       return oat_file;
     }
   }

runtime/oat_file.h
@@ -72,6 +72,10 @@ class OatFile {
     return is_executable_;
   }
 
+  bool IsCreatedInZygote() const {
+    return is_created_in_zygote_;
+  }
+
   ElfFile* GetElfFile() const {
     CHECK_NE(reinterpret_cast<uintptr_t>(elf_file_.get()), reinterpret_cast<uintptr_t>(nullptr))
         << "Cannot get an elf file from " << GetLocation();
@@ -326,6 +330,9 @@ class OatFile {
 
   // Was this oat_file loaded executable?
   const bool is_executable_;
+
+  // Was this file loaded in Zygote?
+  const bool is_created_in_zygote_;
 
   // Backing memory map for oat file during when opened by ElfWriter during initial compilation.
   std::unique_ptr<MemMap> mem_map_;

commit b24d238d312c7628b782282023d41ff27bb7fa6a

[Xposed] Fix oatdump crash
This was a regression from 5184a0542b81c297afa97d4540e4751db5fc9876.

runtime/oat_file.cc
@@ -139,7 +139,7 @@ OatFile* OatFile::OpenElfFile(File* file,
 
 OatFile::OatFile(const std::string& location, bool is_executable)
     : location_(location), begin_(NULL), end_(NULL), is_executable_(is_executable),
-      is_created_in_zygote_(Runtime::Current()->IsZygote()), dlopen_handle_(NULL),
+      is_created_in_zygote_(Runtime::Current() != nullptr && Runtime::Current()->IsZygote()), dlopen_handle_(NULL),
       secondary_lookup_lock_("OatFile secondary lookup lock", kOatFileSecondaryLookupLock) {
   CHECK(!location_.empty());
 }