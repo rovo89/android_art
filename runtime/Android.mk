cc5ebdf29ea47e24e79279169e0192dfc08b38c8
Mathieu Chartier <mathieuc@google.com>
2015-07-27 11:19:43 -0700

    Split the class table for each class loader
    Each class loader now has its own class table. This makes it easier
    to mark classes when a classloader is marked.
    
    Fixed a bug in LookupClass where we used to look ignore the return
    value of InsertClass.
    
    Bug: 22720414
    
    Change-Id: If2cd717989a20a6e245ebec24ad52dc47dd3207d

--- runtime/Android.mk
+++ runtime/Android.mk
@@ -39,6 +39,7 @@ LIBART_COMMON_SRC_FILES := \
   base/unix_file/random_access_file_utils.cc \
   check_jni.cc \
   class_linker.cc \
+  class_table.cc \
   common_throws.cc \
   debugger.cc \
   dex_file.cc \
