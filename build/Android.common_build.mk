7cdcea7ff65f6235f74cd453221b71eb0af7a442
rovo89 <android@robv.de>
2015-09-05 12:32:00 +0200

    [Xposed] Only build for host if explicitly requested

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -29,21 +29,21 @@ include art/build/Android.common.mk
 # Beware that tests may use the non-debug build for performance, notable 055-enum-performance
 #
 ART_BUILD_TARGET_NDEBUG ?= true
-ART_BUILD_TARGET_DEBUG ?= true
-ART_BUILD_HOST_NDEBUG ?= true
-ART_BUILD_HOST_DEBUG ?= true
+ART_BUILD_TARGET_DEBUG ?= false
+ART_BUILD_HOST_NDEBUG ?= false
+ART_BUILD_HOST_DEBUG ?= false
 
 ifeq ($(ART_BUILD_TARGET_NDEBUG),false)
 $(info Disabling ART_BUILD_TARGET_NDEBUG)
 endif
-ifeq ($(ART_BUILD_TARGET_DEBUG),false)
-$(info Disabling ART_BUILD_TARGET_DEBUG)
+ifeq ($(ART_BUILD_TARGET_DEBUG),true)
+$(info Enabling ART_BUILD_TARGET_DEBUG)
 endif
-ifeq ($(ART_BUILD_HOST_NDEBUG),false)
-$(info Disabling ART_BUILD_HOST_NDEBUG)
+ifeq ($(ART_BUILD_HOST_NDEBUG),true)
+$(info Enabling ART_BUILD_HOST_NDEBUG)
 endif
-ifeq ($(ART_BUILD_HOST_DEBUG),false)
-$(info Disabling ART_BUILD_HOST_DEBUG)
+ifeq ($(ART_BUILD_HOST_DEBUG),true)
+$(info Enabling ART_BUILD_HOST_DEBUG)
 endif
 
 #
