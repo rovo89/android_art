3524472e8f064dbd42e1a35d511a1efe36d2cbf2
Richard Uhler <ruhler@google.com>
2015-09-10 16:45:54 -0700

    Generate an hprof file to test ahat.
    This change sets up the infrastructure to write test cases for ahat
    that make use of an hprof file automatically generated from a sample
    program.
    
    Change-Id: Id11f656afb69c96a26655cc4caeb745ad844f431

---
+++ tools/ahat/test-dump/Main.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import dalvik.system.VMDebug;
+import java.io.IOException;
+
+/**
+ * Program used to create a heap dump for test purposes.
+ */
+public class Main {
+  // Keep a reference to the DumpedStuff instance so that it is not garbage
+  // collected before we take the heap dump.
+  public static DumpedStuff stuff;
+
+  // We will take a heap dump that includes a single instance of this
+  // DumpedStuff class. Objects stored as fields in this class can be easily
+  // found in the hprof dump by searching for the instance of the DumpedStuff
+  // class and reading the desired field.
+  public static class DumpedStuff {
+    public String basicString = "hello, world";
+    public String nullString = null;
+    public Object anObject = new Object();
+  }
+
+  public static void main(String[] args) throws IOException {
+    if (args.length < 1) {
+      System.err.println("no output file specified");
+      return;
+    }
+    String file = args[0];
+
+    // Allocate the instance of DumpedStuff.
+    stuff = new DumpedStuff();
+
+    // Take a heap dump that will include that instance of DumpedStuff.
+    System.err.println("Dumping hprof data to " + file);
+    VMDebug.dumpHprofData(file);
+  }
+}
