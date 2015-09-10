2917841a7dddec7b9102b87465e0b33bf1969575
rovo89 <android@robv.de>
2015-09-10 11:54:37 +0200

    [Xposed] Compile return-void-barrier
    It seems to be an uncommon instruction, but it can easily be compiled.
    Cherry-picked from https://android-review.googlesource.com/#/c/123156/.
    
    Fixes rovo89/Xposed#60.

--- compiler/dex/mir_dataflow.cc
+++ compiler/dex/mir_dataflow.cc
@@ -375,7 +375,7 @@ const uint64_t MIRGraph::oat_data_flow_attributes_[kMirOpLast] = {
   // 72 INVOKE_INTERFACE {vD, vE, vF, vG, vA}
   DF_FORMAT_35C | DF_NULL_CHK_OUT0 | DF_UMS,
 
-  // 73 UNUSED_73
+  // 73 RETURN_VOID_BARRIER
   DF_NOP,
 
   // 74 INVOKE_VIRTUAL_RANGE {vCCCC .. vNNNN}
