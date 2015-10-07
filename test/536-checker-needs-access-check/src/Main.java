386062d13ce20d036555a9e24b73a67b4156b5cb
Calin Juravle <calin@google.com>
2015-10-07 18:55:43 +0100

    Make sure classes with different access checks are not GVN-ed
    Change-Id: I89f72fef3be35a4dd9585d97d03a3150386e0891

--- test/536-checker-needs-access-check/src/Main.java
+++ test/536-checker-needs-access-check/src/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2015 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,9 @@
  */
 
 import other.InaccessibleClass;
+import other.InaccessibleClassProxy;
 
 public class Main {
-    public static final boolean VERBOSE = false;
-
     public static void main(String[] args) {
         try {
             testInstanceOf();
@@ -36,6 +35,12 @@ public class Main {
             testCheckCastNull();
         } catch (IllegalAccessError e) {
             System.out.println("Got expected error checkcast null");
+        }
+
+        try {
+            testDontGvnLoadClassWithAccessChecks(new Object());
+        } catch (IllegalAccessError e) {
+            System.out.println("Got expected error instanceof (keep LoadClass with access check)");
         }
     }
 
@@ -57,6 +62,20 @@ public class Main {
     /// CHECK: CheckCast
     public static InaccessibleClass testCheckCastNull() {
         return (InaccessibleClass) null;
+    }
+
+    /// CHECK-START: boolean Main.testDontGvnLoadClassWithAccessChecks(java.lang.Object) inliner (before)
+    /// CHECK: InvokeStaticOrDirect
+
+    /// CHECK-START: boolean Main.testDontGvnLoadClassWithAccessChecks(java.lang.Object) inliner (after)
+    /// CHECK-NOT: InvokeStaticOrDirect
+
+    /// CHECK-START: boolean Main.testDontGvnLoadClassWithAccessChecks(java.lang.Object) GVN (after)
+    /// CHECK: LoadClass needs_access_check:false
+    /// CHECK: LoadClass needs_access_check:true
+    public static boolean testDontGvnLoadClassWithAccessChecks(Object o) {
+        InaccessibleClassProxy.test(o);
+        return ic instanceof InaccessibleClass;
     }
 
     public static InaccessibleClass ic;
