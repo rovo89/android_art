multi-os-engine/art

ART: Implement DeadPhiHandling in PrimitiveTypePropagation
DeadPhiHandling revives non-conflicting phis with environment uses
but does not properly merge types. To not duplicate code, this patch
modifies PrimitiveTypePropagation to deal with conflicts and thus
replaces DeadPhiHandling altogether.

Bug: 24252151
Bug: 24252100

Change-Id: I198c71d1b8167fc05783a5a24aa9f1e3804acafe

compiler/optimizing/primitive_type_propagation.h
@@ -38,6 +38,8 @@ class PrimitiveTypePropagation : public ValueObject {
   void AddToWorklist(HPhi* phi);
   void AddDependentInstructionsToWorklist(HInstruction* instruction);
   bool UpdateType(HPhi* phi);
+  bool TypePhiFromInputs(HPhi* phi);
+  bool ReplacePhiInputs(HPhi* phi);
 
   HGraph* const graph_;
   ArenaVector<HPhi*> worklist_;

rovo89/android_art

compiler/optimizing/primitive_type_propagation.h
@@ -38,6 +38,8 @@ class PrimitiveTypePropagation : public ValueObject {
   void AddToWorklist(HPhi* phi);
   void AddDependentInstructionsToWorklist(HInstruction* instruction);
   bool UpdateType(HPhi* phi);
+  bool TypePhiFromInputs(HPhi* phi);
+  bool TypeInputsOfPhi(HPhi* phi);
 
   HGraph* const graph_;
   ArenaVector<HPhi*> worklist_;