c07d66d816924bf97fa97192c23670010e775a3c
Alex Light <allight@google.com>
2015-11-13 10:51:10 -0800

    Add ability to skip run-tests with ART_TEST_RUN_TEST_SKIP.
    One can skip running specific run-tests under any configuration by
    setting the ART_TEST_RUN_TEST_SKIP variable to a list of the run-test
    names to skip.
    
        $ # None of the variants of 022-interfaces will be run.
        $ ART_TEST_RUN_TEST_SKIP=022-interface mm test-art-host-run-test
    
    Change-Id: I33f24dd7f1eb9f4d6dc407a4abc3c5919437d4fa

--- test/Android.run-test.mk
+++ test/Android.run-test.mk
@@ -208,6 +208,10 @@ define name-to-var
 $(shell echo $(1) | tr '[:lower:]' '[:upper:]' | tr '-' '_')
 endef  # name-to-var
 
+ART_TEST_KNOWN_BROKEN += $(call all-run-test-names,$(TARGET_TYPES),$(RUN_TYPES),$(PREBUILD_TYPES), \
+        $(COMPILER_TYPES),$(RELOCATE_TYPES),$(TRACE_TYPES),$(GC_TYPES),$(JNI_TYPES), \
+        $(IMAGE_TYPES), $(PICTEST_TYPES), $(DEBUGGABLE_TYPES), $(ART_TEST_RUN_TEST_SKIP), $(ALL_ADDRESS_SIZES))
+
 # Tests that are timing sensitive and flaky on heavily loaded systems.
 TEST_ART_TIMING_SENSITIVE_RUN_TESTS := \
   053-wait-some \
