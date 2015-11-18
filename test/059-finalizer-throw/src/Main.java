13ca74fed068a6a49221b5213ce0b1bebeda3ed6
Narayan Kamath <narayan@google.com>
2015-11-18 15:42:26 +0000

    Fix 030-bad-finalizer & 059-finalizer-throw.
    Use a default uncaught exception handler to log messages.
    
    bug: 25759624
    Change-Id: I9f3253261e15cd62bd433e01b852ecae40a8f58d

--- test/059-finalizer-throw/src/Main.java
+++ test/059-finalizer-throw/src/Main.java
@@ -30,6 +30,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(
+            new Thread.UncaughtExceptionHandler() {
+                @Override
+                public void uncaughtException(Thread t, Throwable e) {
+                    System.out.println("Caught exception: " + e.getMessage());
+                }
+            });
+
         createAndForget();
 
         System.gc();
@@ -56,8 +64,6 @@ public class Main {
         } catch (InterruptedException ie) {
             System.err.println(ie);
         }
-
-        System.out.println("done");
     }
 
     protected void finalize() throws Throwable {
