788f2f05c3e5b0e5bda247b00e34f0094585546f
Nicolas Geoffray <ngeoffray@google.com>
2016-01-22 12:41:38 +0000

    Revert "Revert "Inline methods with loops.""
    Bug: 26689526
    
    This reverts commit 451ad8d1be9a1949ea3c3e3a713a9e76198a8b2d.
    
    Change-Id: If484fe4c0744254dd7568fd5006e574d621a1855

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -689,6 +689,10 @@ class HLoopInformation : public ArenaObject<kArenaAllocLoopInfo> {
   void Add(HBasicBlock* block);
   void Remove(HBasicBlock* block);
 
+  void ClearAllBlocks() {
+    blocks_.ClearAllBits();
+  }
+
  private:
   // Internal recursive implementation of `Populate`.
   void PopulateRecursive(HBasicBlock* block);
