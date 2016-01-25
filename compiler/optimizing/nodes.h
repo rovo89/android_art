2f10a5fb8c236a6786928f0323bd312c3ee9a4cc
Mark P Mendell <mark.p.mendell@intel.com>
2016-01-25 14:47:50 +0000

    Revert "Revert "X86: Use the constant area for more operations.""
    This reverts commit cf8d1bb97e193e02b430d707d3b669565fababb4.
    
    Handle the case of an intrinsic where CurrentMethod is still an input.
    This will be the case when there are unresolved classes in the
    hierarchy.
    
    Add a test case to confirm that we don't crash when handling Math.abs,
    which wants to add a pointer to the constant area for the bitmask to be
    used to remove the sign bit.
    
    Enhance 565-checker-condition-liveness to check for the case of deeply
    nested EmitAtUseSite chains.
    
    Change-Id: I022e8b96a32f5bf464331d0c318c56b9d0ac3c9a

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -1259,6 +1259,7 @@ class HLoopInformationOutwardIterator : public ValueObject {
 #define FOR_EACH_CONCRETE_INSTRUCTION_X86(M)                            \
   M(X86ComputeBaseMethodAddress, Instruction)                           \
   M(X86LoadFromConstantTable, Instruction)                              \
+  M(X86FPNeg, Instruction)                                              \
   M(X86PackedSwitch, Instruction)
 #endif
 
