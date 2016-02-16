multi-os-engine/art

Support CMOV for x86 Select
If possible, generate CMOV to implement HSelect.  Tricky cases are a
long or FP condition (no single CC generated), FP inputs (no FP CMOV)
and when the condition is a boolean or not emitted at the use site.
In these cases, keep using the existing HSelect code.

Renamed pc_relative_fixups_x86.cc to
prepare_for_register_allocation_x86.cc and added code to detect the
case of long compare and long Select, recreating an HIf to remove
the need for the long compare to be materialized.  This was previously
forced in prepare_for_register_allocation.cc.

Allow X86 long compare to accept stack operands.

Change-Id: Ib0a5a84c939213c46fec3c0a2622e87a83aaa33e
Signed-off-by: Mark Mendell <mark.p.mendell@intel.com>

compiler/Android.mk
@@ -89,8 +89,8 @@ LIBART_COMPILER_SRC_FILES := \
 	optimizing/optimization.cc \
 	optimizing/optimizing_compiler.cc \
 	optimizing/parallel_move_resolver.cc \
-	optimizing/pc_relative_fixups_x86.cc \
 	optimizing/prepare_for_register_allocation.cc \
+	optimizing/prepare_for_register_allocation_x86.cc \
 	optimizing/reference_type_propagation.cc \
 	optimizing/register_allocator.cc \
 	optimizing/select_generator.cc \

rovo89/android_art

Change-Id: I4ff1e152b8ef126fbbabeb3316e9e2b6a6b74aeb

compiler/Android.mk
@@ -90,7 +90,6 @@ LIBART_COMPILER_SRC_FILES := \
 	optimizing/optimization.cc \
 	optimizing/optimizing_compiler.cc \
 	optimizing/parallel_move_resolver.cc \
-	optimizing/pc_relative_fixups_x86.cc \
 	optimizing/prepare_for_register_allocation.cc \
 	optimizing/reference_type_propagation.cc \
 	optimizing/register_allocator.cc \
@@ -182,6 +181,7 @@ LIBART_COMPILER_SRC_FILES_x86 := \
 	linker/x86/relative_patcher_x86_base.cc \
 	optimizing/code_generator_x86.cc \
 	optimizing/intrinsics_x86.cc \
+	optimizing/pc_relative_fixups_x86.cc \
 	utils/x86/assembler_x86.cc \
 	utils/x86/managed_register_x86.cc \
 