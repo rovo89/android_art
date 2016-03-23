2190d929695c31ad7195e2c366f4102836a7d827
Alex Light <allight@google.com>
2016-03-23 10:20:41 -0700

    Add support for Dex version 37 in Runtime.
    We are skipping version 36 of the dex file format due to a bug in
    Dalvik dating back to ICS where dex files marked version 036 would
    erroneously be accepted.
    
    Bug: 27538761
    Bug: 27809626
    
    (cherry picked from commit c49618160d5fa64ca4883d2e09fa34b83491c8ea)
    
    Change-Id: Ic053f7e25f5a8c3df83ff34b6656528824b2df12

--- runtime/dex_file.cc
+++ runtime/dex_file.cc
@@ -62,7 +62,12 @@
 namespace art {
 
 const uint8_t DexFile::kDexMagic[] = { 'd', 'e', 'x', '\n' };
-const uint8_t DexFile::kDexMagicVersion[] = { '0', '3', '5', '\0' };
+const uint8_t DexFile::kDexMagicVersions[DexFile::kNumDexVersions][DexFile::kDexVersionLen] = {
+  {'0', '3', '5', '\0'},
+  // Dex version 036 skipped because of an old dalvik bug on some versions of android where dex
+  // files with that version number would erroneously be accepted and run.
+  {'0', '3', '7', '\0'}
+};
 
 bool DexFile::GetChecksum(const char* filename, uint32_t* checksum, std::string* error_msg) {
   CHECK(checksum != nullptr);
@@ -493,7 +498,12 @@ bool DexFile::IsMagicValid(const uint8_t* magic) {
 
 bool DexFile::IsVersionValid(const uint8_t* magic) {
   const uint8_t* version = &magic[sizeof(kDexMagic)];
-  return (memcmp(version, kDexMagicVersion, sizeof(kDexMagicVersion)) == 0);
+  for (uint32_t i = 0; i < kNumDexVersions; i++) {
+    if (memcmp(version, kDexMagicVersions[i], kDexVersionLen) == 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
 uint32_t DexFile::GetVersion() const {
