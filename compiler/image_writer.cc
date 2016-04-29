c4e46909a221d6dc5efabf240c02e5afe7e4c6cb
Brian Carlstrom <bdc@google.com>
2016-04-29 10:28:34 -0700

    Add null checks to weak DexCache references
    Bug: 28452385
    Change-Id: I88243af47b8e091c360927d1bdb867770f092475

--- compiler/image_writer.cc
+++ compiler/image_writer.cc
@@ -1029,6 +1029,9 @@ ObjectArray<Object>* ImageWriter::CreateImageRoots(size_t oat_index) const {
     for (const ClassLinker::DexCacheData& data : class_linker->GetDexCachesData()) {
       mirror::DexCache* dex_cache =
           down_cast<mirror::DexCache*>(self->DecodeJObject(data.weak_root));
+      if (dex_cache == nullptr) {
+        continue;
+      }
       const DexFile* dex_file = dex_cache->GetDexFile();
       if (!IsInBootImage(dex_cache)) {
         dex_cache_count += image_dex_files.find(dex_file) != image_dex_files.end() ? 1u : 0u;
@@ -1045,6 +1048,9 @@ ObjectArray<Object>* ImageWriter::CreateImageRoots(size_t oat_index) const {
     for (const ClassLinker::DexCacheData& data : class_linker->GetDexCachesData()) {
       mirror::DexCache* dex_cache =
           down_cast<mirror::DexCache*>(self->DecodeJObject(data.weak_root));
+      if (dex_cache == nullptr) {
+        continue;
+      }
       const DexFile* dex_file = dex_cache->GetDexFile();
       if (!IsInBootImage(dex_cache)) {
         non_image_dex_caches += image_dex_files.find(dex_file) != image_dex_files.end() ? 1u : 0u;
@@ -1056,6 +1062,9 @@ ObjectArray<Object>* ImageWriter::CreateImageRoots(size_t oat_index) const {
     for (const ClassLinker::DexCacheData& data : class_linker->GetDexCachesData()) {
       mirror::DexCache* dex_cache =
           down_cast<mirror::DexCache*>(self->DecodeJObject(data.weak_root));
+      if (dex_cache == nullptr) {
+        continue;
+      }
       const DexFile* dex_file = dex_cache->GetDexFile();
       if (!IsInBootImage(dex_cache) && image_dex_files.find(dex_file) != image_dex_files.end()) {
         dex_caches->Set<false>(i, dex_cache);
