41be823ff1db81602d4bc28e5917a7aa7c8a26d0
Mathieu Chartier <mathieuc@google.com>
2016-06-15 15:07:03 -0700

    Propagate flags to ShouldHaveEmbeddedVTable
    Pass kVerifyFlags and kReadBarrierrOption to ShouldHaveEmbeddedVTable
    from ShouldHaveImt.
    
    Bug: 29188168
    
    (cherry picked from commit efeb9f01308c5c4b723ff905cf5bea1636f7a549)
    
    Change-Id: Ia76d8cd340fbb0527955f4ef611d6e14bebf8dd9

--- runtime/mirror/class.h
+++ runtime/mirror/class.h
@@ -831,7 +831,8 @@ class MANAGED Class FINAL : public Object {
            ReadBarrierOption kReadBarrierOption = kWithReadBarrier>
   bool ShouldHaveImt() SHARED_REQUIRES(Locks::mutator_lock_) {
     return ShouldHaveEmbeddedVTable<kVerifyFlags, kReadBarrierOption>() &&
-        GetIfTable() != nullptr && !IsArrayClass();
+        GetIfTable<kVerifyFlags, kReadBarrierOption>() != nullptr &&
+        !IsArrayClass<kVerifyFlags, kReadBarrierOption>();
   }
 
   template<VerifyObjectFlags kVerifyFlags = kDefaultVerifyFlags,
