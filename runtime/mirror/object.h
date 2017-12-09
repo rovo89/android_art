3718567a726c0b86b98652fd9d2add9d4fd008fc
rovo89 <android@robv.de>
2017-12-09 10:20:42 +0100

    [Xposed] Clone objects directly to the target class
    Cloning an object with a different size causes some checks to fail. Even
    though the proper target class will be set almost immediately, this
    sitation can occur, especially under high load and with certain debug
    features enabled. So better create the new object with the correct
    class. Requires a change in the Xposed project as well.

--- runtime/mirror/object.h
+++ runtime/mirror/object.h
@@ -102,8 +102,7 @@ class MANAGED LOCKABLE Object {
            ReadBarrierOption kReadBarrierOption = kWithReadBarrier>
   size_t SizeOf() SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
 
-  Object* Clone(Thread* self) SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
-  Object* Clone(Thread* self, size_t num_target_bytes) SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
+  Object* Clone(Thread* self, Class* target_klass = nullptr) SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
 
   int32_t IdentityHashCode() const SHARED_LOCKS_REQUIRED(Locks::mutator_lock_);
 
